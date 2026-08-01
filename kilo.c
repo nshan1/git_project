aa494beb120a12adea118e93042fbfcb68db3f16
nshan1 <nshanhakobyan700@gmail.com>
2026-08-01 13:23:54 +0400

    01-08-26

--- kilo.c
+++ kilo.c
@@ -10,11 +10,13 @@
 #include <termios.h>
 
 /*** defines ***/
+#define KILO_VERSION "0.0.1"
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 /*** data ***/
 
 struct editorConfig{
+    int cx, cy;
 	int screenrows;
 	int screencols;
 	struct termios orig_termios;
@@ -121,8 +123,22 @@ void abFree(struct abuf *ab){
 void editorDrawRows(struct abuf *ab){
 	int y;
 	for(y = 0; y < E.screenrows; y++){
-        abAppend(ab, "~", 1);
+	    if(y == E.screenrows /3){
+		    char welcome[80];
+			int welcomelen = snprintf(welcome, sizeof(welcome), "Kilo editor -- version %s", KILO_VERSION);
+			if(welcomelen > E.screencols) welcomelen = E.screencols;
+			int padding = (E.screencols - welcomelen) / 2;
+			if(padding){
+                abAppend(ab, "~", 1);
+                padding--;
+			}
+			while(padding--) abAppend(ab, " ", 1);
+			abAppend(ab, welcome, welcomelen);
+		} else {
+		    abAppend(ab, "~", 1);
+		}
 
+        abAppend(ab, "\x1b[K", 3);
         if(y < E.screenrows -1){
             abAppend(ab, "\r\n", 2);
 		}
@@ -131,14 +147,36 @@ void editorDrawRows(struct abuf *ab){
 
 void editorRefreshScreen(){
     struct abuf ab = ABUF_INIT;
-    abAppend(&ab, "\x1b[2J", 4);
+    abAppend(&ab, "\x1b[?25l", 6);
     abAppend(&ab, "\x1b[H", 3);
 
 	editorDrawRows(&ab);
+	char buf[32];
+	snprintf(buf, sizeof(buf), "\x1b[%d;%dH", E.cy+1, E.cx+1);
 
-	abAppend(&ab, "\x1b[H", 3);
+	abAppend(&ab, buf, strlen(buf));
+	abAppend(&ab, "\x1b[?25h", 6);
 	write(STDOUT_FILENO, ab.b, ab.len);
 	abFree(&ab);
+}
+
+/*** input ***/
+
+void editorMoveCursor(char key){
+    switch (key) {
+        case 'a':
+            E.cx--;
+            break;
+        case 'd':
+            E.cx++;
+            break;
+        case 'w':
+            E.cy--;
+            break;
+        case 's':
+            E.cy++;
+            break;
+    }
 }
 
 void editorProcessKeypress(){
@@ -150,12 +188,21 @@ void editorProcessKeypress(){
 		write(STDOUT_FILENO, "\x1b[H", 3);
 		exit(0);
 		break;
+
+		case 'w':
+		case 's':
+		case 'a':
+		case 'd':
+            editorMoveCursor(c);
+            break;
 	}
 }
 
 /*** init ***/
 
 void initEditor(){
+    E.cx = 0;
+    E.cy = 0;
 	if(getWindowSize(&E.screenrows, &E.screencols) == -1) die("getWindowSize");
 }
 
@@ -171,4 +218,4 @@ int main()
 	}
 	return 0;
 }
-// STEP 39
+// STEP 46
